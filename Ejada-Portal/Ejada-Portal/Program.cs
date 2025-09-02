a54041e73bcc9ae06b80f49fc91cacca1ece0c48
EJADA\malkhalaileh <malkhalaileh@ejada.com>
2025-09-02 14:54:16 +0300

    comment

--- Ejada-Portal/Ejada-Portal/Program.cs
+++ Ejada-Portal/Ejada-Portal/Program.cs
@@ -3,7 +3,4 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.Run();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//commint from vs code
+app.Run();  
